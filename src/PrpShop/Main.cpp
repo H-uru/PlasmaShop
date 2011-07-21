3b8e28321536c4d0b85441152eb2afbad37cfc7a
Michael Hansen <zrax0111@gmail.com>
2011-07-20 23:56:43 -0700

    Update build number to distinguish differences

--- src/PrpShop/Main.cpp
+++ src/PrpShop/Main.cpp
@@ -40,7 +40,7 @@
 #include "PRP/QCreatable.h"
 #include "QPrcEditor.h"
 
-#define PRPSHOP_VERSION "Build 144"
+#define PRPSHOP_VERSION "Build 152"
 
 PrpShopMain* PrpShopMain::sInstance = NULL;
 PrpShopMain* PrpShopMain::Instance() { return sInstance; }
